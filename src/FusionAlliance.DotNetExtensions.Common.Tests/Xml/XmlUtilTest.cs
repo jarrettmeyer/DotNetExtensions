3f9c02395c4d093dcc9d96d999e8c4c0f1e0bf05
Jarrett Meyer <jarrettmeyer@gmail.com>
2014-07-24 15:59:44 -0400

    Adds XML utility methods.

---
+++ src/FusionAlliance.DotNetExtensions.Common.Tests/Xml/XmlUtilTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using FusionAlliance.DotNetExtensions.Common.Xml;
+using NUnit.Framework;
+
+namespace FusionAlliance.DotNetExtensions.Common.Tests.Xml
+{
+    [TestFixture]
+    public class XmlUtilTest
+    {
+        [Test]
+        public void XmlToObject_can_convert_a_string_into_an_object()
+        {
+            DemoObject obj = XmlUtil.XmlToObject<DemoObject>("<DemoObject><Name>John Doe</Name><Age>99</Age></DemoObject>");
+            Assert.AreEqual("John Doe", obj.Name);
+            Assert.AreEqual(99, obj.Age);
+        }
+
+        [Test]
+        public void XmlToObject_throws_an_argument_null_exception_when_the_string_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => { XmlUtil.XmlToObject<DemoObject>((string)null); });
+        }
+
+        public class DemoObject
+        {
+            public string Name { get; set; }
+
+            public int Age { get; set; }
+        }
+    }
+}
---
+++ src/FusionAlliance.DotNetExtensions.Common/Xml/XmlExtenstions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace FusionAlliance.DotNetExtensions.Common.Xml
+{
+    public static class XmlExtenstions
+    {
+        public static XDocument ToXml<T>(this T source)
+            where T : class
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            return XmlUtil.ObjectToXml(source);
+        }
+    }
+}
