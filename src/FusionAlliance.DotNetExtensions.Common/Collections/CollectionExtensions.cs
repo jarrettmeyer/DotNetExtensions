cb5db0bdc1f088b2c81dd6a75834b285524ab2c1
Jarrett Meyer <jarrettmeyer@gmail.com>
2014-07-26 17:15:45 -0400

    starts collections extensions

---
+++ src/FusionAlliance.DotNetExtensions.Common/Collections/CollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FusionAlliance.DotNetExtensions.Common.Collections
+{
+    public static class CollectionExtensions
+    {
+        public static bool IsEmpty<T>(this IEnumerable<T> collection)
+        {
+            return !collection.Any();
+        }
+
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> collection)
+        {
+            return collection == null || collection.IsEmpty();
+        }
+    }
+}
