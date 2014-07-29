7da538d7e7a1a7401d09bd1dc82eba6f35f04e5b
Jarrett Meyer <jarrettmeyer@gmail.com>
2014-07-29 09:47:40 -0400

    Adds web response extension methods.

---
+++ src/FusionAlliance.DotNetExtensions.Common.Tests/Net/WebResponseExtensionTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using FusionAlliance.DotNetExtensions.Common.Net;
+using NUnit.Framework;
+
+namespace FusionAlliance.DotNetExtensions.Common.Tests.Net
+{
+    [TestFixture, Category("Network")]
+    public class WebResponseExtensionTests : IDisposable
+    {
+        private WebRequest request;
+        private WebResponse response;
+        private byte[] responseBytes;
+        private string responseString;
+
+        [SetUp]
+        public void Before_each_test()
+        {
+            request = WebRequest.Create("http://www.fusionalliance.com");
+            response = request.GetResponse();
+        }
+
+        [Test]
+        public void ReadResponseStreamToBytes_can_return_a_buffer()
+        {
+            responseBytes = response.ReadResponseStreamToBytes();
+            Assert.Greater(responseBytes.Length, 0);
+        }
+
+        [Test]
+        public void ReadResponseStreamToString_can_return_a_string()
+        {
+            responseString = response.ReadResponseStreamToString();
+            StringAssert.IsMatch(@"\<!doctype html\>", responseString);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool isDisposing)
+        {
+            if (isDisposing)
+            {
+                response.Dispose();
+            }
+        }
+    }
+}
---
+++ src/FusionAlliance.DotNetExtensions.Common/Net/WebResponseExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+namespace FusionAlliance.DotNetExtensions.Common.Net
+{
+    public static class WebResponseExtensions
+    {
+        public static byte[] ReadResponseStreamToBytes(this WebResponse response)
+        {
+            long contentLength = response.ContentLength;
+            int bufferLength = contentLength < int.MaxValue ? (int)contentLength : int.MaxValue;
+            if (bufferLength < 0)
+            {
+                throw new InvalidOperationException("Unable to get content length from web response.");
+            }
+            byte[] buffer = new byte[bufferLength];
+            using (var stream = response.GetResponseStream())
+            {
+                if (stream == null)
+                {
+                    throw new NullReferenceException("Unable to get response stream from web response.");
+                }
+                stream.Read(buffer, 0, bufferLength);
+                return buffer;
+            }
+        }
+
+        public static string ReadResponseStreamToString(this WebResponse response, Encoding encoding = null)
+        {
+            encoding = encoding ?? Encoding.UTF8;
+            var bytes = response.ReadResponseStreamToBytes();
+            var responseString = encoding.GetString(bytes);
+            return responseString;
+        }
+    }
+}
